8ff10026fdf99fac152b52544fbd5f56796178c2
Jeronimo Cardozo <jdcm182@gmail.com>
2022-09-16 14:04:57 -0300

    Diet type filter working w/bugs

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -12,7 +12,24 @@ export default function Filter() {
 
     const dispatch = useDispatch();
 
-    let tipos = []; // [{type, count}, {type, count}, {type, count}, {type, count}, {type, count}]
+    /*
+    recipes.forEach(r => {
+        r.diets.forEach((d) => { countDietTypes(types, d) });
+    });
+    console.log('types:  ', types)
+
+    function countDietTypes(arr, diet) {
+        let found = arr.find(obj => obj.type === diet);
+        if (found.hasOwnProperty('count')) {
+            found.count++;
+        }
+        else {
+            found.count = 0;
+        }
+    }
+    */
+
+    let typeCount = []; // [{type, count}, {type, count}, {type, count}, {type, count}, {type, count}]
     function addDietType(arr, key, diet) {
         let found = arr.find(obj => obj[key] === diet);
         //found ? found.count++ : found = { key: diet, count: 0 }   // <-- key es variable!! como la uso con esta sintaxis?
@@ -20,27 +37,35 @@ export default function Filter() {
         else {
             found = {};
             found[key] = diet;
-            found.count = 0;
+            found.count = 1;
             arr.push(found);
-        }
+        } // agregar name
 
-        // let found = tipos.find(obj => obj['type'] === diet);
+        // let found = typeCount.find(obj => obj['type'] === diet);
         // found ? found.count++ : found = { type: diet, count: 0 }
         // console.log('found: ', found)
-        // tipos.push(found);
+        // typeCount.push(found);
     }
 
+    //function countRecipes(arr,)
+
     recipes.forEach(r => {
-        //r.diets && tipos.push(r.diets[0]);
-        r.diets.forEach((d) => { addDietType(tipos, 'type', d) })
+        //r.diets && typeCount.push(r.diets[0]);
+        r.diets.forEach((d) => { addDietType(typeCount, 'type', d) }) // add name
     });
 
+    typeCount.forEach((t) => {
+        const found = types.find((f) => f.type === t)
+        if (found) found.count = t.count;
+    })
+
+    console.log('typeCount: ', typeCount)
+    console.log('types: ', types)
     /*
         var result = jsObjects.find(obj => {
           return obj.b === 6
         })
     */
-
 
     return (
         <div className={style.filters}>
@@ -52,24 +77,54 @@ export default function Filter() {
                     className={[style.fil, t.filter ? style.selectedF : style.normalF].join(' ')}
                     onClick={() => { /* alert(t.name) */
                         toggleFilter(types, t.type);
-                        dispatch(setFilters(filterByType(t.type)));
+                        //dispatch(setFilters(filterByType(t.type)));
+                        dispatch(setFilters(filterByTypes(getActiveFilters())));
                     }}>
 
-                    {t.name/* +tipos.find(tp=>tp.name===) */} < br />
+                    { }
+                    {t.name + ` (${getCount(t.type)})`}
+                    < br />
                 </div>
             ))}
 
             <br />
 
-            {tipos.map((t, i) => (<div key={'t' + i}>{t.type + `(${t.count})`}</div>))}
+            {typeCount.map((t, i) => (<div key={'t' + i}>{t.type + `(${t.count})`}</div>))}
 
         </div >
-
     )
+    //{t.count ? t.name + ` (${t.count})` : t.name} < br />
+
+    function getCount(type) {
+        let res = 0;
+        const found = typeCount.find(tc => tc.type === type)
+        if (found) res = found.count;
+        return res;
+    }
+
+    function getActiveFilters() {
+        const res = [];
+        types.forEach(t => {
+            if (t.filter === true) res.push(t.type)
+        })
+        return res;
+    }
+
+    function filterByTypes(typesArray) {
+        //return recipes.filter(r => r.diets.includes(type))
+        let res = recipes;
+
+        typesArray.forEach(type => {
+            res = res.filter(r => r.diets.includes(type))
+        });
+
+        return res;
+    }
 
     function filterByType(type) {
         return recipes.filter(r => r.diets.includes(type))
     }
+
 
     function toggleFilter(arr, type) {
         let itemFound = arr.find(obj => obj.type === type);
